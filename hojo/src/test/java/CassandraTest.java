2cb16e7c009ff00f295aad28905b975cc28e9765
p0s00gu <prasanna.subburaj@walmart.com>
2017-04-26 15:37:04 -0500

    changes made to take the nativeports

--- hojo/src/test/java/CassandraTest.java
+++ hojo/src/test/java/CassandraTest.java
@@ -11,22 +11,25 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
-
 public class CassandraTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(CassandraTest.class);
     public static Cluster cluster;
+    Session session=null;
 
     @Before
     public void loadData() {
         cluster = com.datastax.driver.core.Cluster.builder()
-                .addContactPoints("localhost")
+                .addContactPoints("127.0.0.1")
                 .withPort(Integer.valueOf("9042"))
                 .build();
+        session=cluster.connect("inventory");
+
     }
 
 
     @Test
     public void generateMessages1(){
-        KeyspaceMetadata keyspaceCql = cluster.getMetadata().getKeyspace("\"inventory\"");
+        //KeyspaceMetadata keyspaceCql = cluster.getMetadata().getKeyspace("\"inventory\"");
+        session.execute("select * from inventory.emp;");
     }
 }
